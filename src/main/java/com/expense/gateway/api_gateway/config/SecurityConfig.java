963175801fe744673248ca59ce737eca1badf343
Vipinkumar <tvkumarvipin@gmail.com>
2026-01-19 14:54:27 +0530

    fix security block issue cors

--- src/main/java/com/expense/gateway/api_gateway/config/SecurityConfig.java
+++ src/main/java/com/expense/gateway/api_gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.expense.gateway.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -18,6 +19,7 @@ public class SecurityConfig {
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
             .authorizeExchange(ex -> ex
             	    .pathMatchers(
+            	    		HttpMethod.OPTIONS, "/**",
             	        "/actuator/health",
             	        "/auth/login",
             	        "/auth/register"
